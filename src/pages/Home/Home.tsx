1687d71991dafbcc211cda27627bc8d1ff375877
Hector <torrezhectorb@gmail.com>
2023-07-17 18:37:01 -0600

    add stateChange

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,24 @@
+import { useDispatch } from 'react-redux'
 import { DesktopNavbar, Header, MenuLeft } from './components'
+import { useEffect } from 'react'
+import { supabase } from '../../supabase/client'
+import { getSession } from '../../store/auth/thunk'
+import { type Session } from '@supabase/supabase-js'
+import { type ThunkDispatch, type Action } from '@reduxjs/toolkit'
+import { type RootState } from '../../store'
+
 export const Home: React.FC = () => {
+  const dispatch: ThunkDispatch<RootState, unknown, Action> = useDispatch()
+
+  useEffect(() => {
+    void supabase.auth.getSession().then(({ data: { session } }) => {
+      void dispatch(getSession(session))
+    })
+    supabase.auth.onAuthStateChange((_event, session: Session | null) => {
+      void dispatch(getSession(session))
+    })
+  }, [])
+
   return (
     <>
     {/* <MobileNavbar/> */}
