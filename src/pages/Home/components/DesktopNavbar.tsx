a3669acdc20b6cf3c2276cb2f1bb811f4e463afe
Hector <torrezhectorb@gmail.com>
2023-07-20 19:36:54 -0600

    feature/ new component

--- src/pages/Home/components/DesktopNavbar.tsx
+++ src/pages/Home/components/DesktopNavbar.tsx
@@ -1,8 +1,7 @@
 import { HiOutlineMail } from 'react-icons/hi'
 import { BiWallet, BiNotification } from 'react-icons/bi'
 import { CiSearch } from 'react-icons/ci'
-import ReactNiceAvatar, { type AvatarFullConfig, genConfig } from 'react-nice-avatar'
-import { Link, NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { type RootState } from '../../../store'
 import { useState } from 'react'
@@ -10,28 +9,11 @@ import { supabase } from '../../../supabase/client'
 import { logOut } from '../../../store/auth/thunk'
 import { type ThunkDispatch } from 'redux-thunk'
 import { type Action } from '@reduxjs/toolkit'
+import { Profile } from './Profile'
 
-const config: AvatarFullConfig = {
-  sex: 'woman',
-  faceColor: '#F9C9B6',
-  earSize: 'big',
-  eyeStyle: 'smile',
-  noseStyle: 'round',
-  mouthStyle: 'smile',
-  shirtStyle: 'short',
-  glassesStyle: 'none',
-  hairColor: '#000',
-  hairStyle: 'womanLong',
-  hatStyle: 'none',
-  hatColor: '#fff',
-  eyeBrowStyle: 'upWoman',
-  shirtColor: '#F4D150',
-  bgColor: '#FFEDEF'
-}
 export const DesktopNavbar: React.FC = () => {
   const [isHovered, setIsHovered] = useState(false)
 
-  const myConfig = genConfig(config)
   const { login, email } = useSelector((state: RootState) => state.auth)
 
   const dispatch: ThunkDispatch<RootState, unknown, Action> = useDispatch()
@@ -47,55 +29,26 @@ export const DesktopNavbar: React.FC = () => {
   }
 
   return (
-    <header className='flex justify-between pt-4 px-5 shadow-md items-center'>
-        <section className='flex gap-8 h-14 items-center  list-none cursor-pointer font-bold'>
+    <header className='flex flex-col  w-screen items-center  pt-4  shadow-md  md:flex-row md:justify-between md:px-2  '>
+        <section className='hidden gap-5 h-14 w-2/4 items-center  list-none cursor-pointer font-bold md:flex'>
             <NavLink to={'/'} className={({ isActive }) => isActive ? 'pb-4 border-b-2  border-b-buttons' : 'pb-4 hover:border-b-2'}>Dashboard</NavLink>
-            <NavLink to={'/about-us'} className={({ isActive }) => isActive ? 'pb-4 border-b-2  border-b-buttons' : 'pb-4 hover:border-b-2'}>About Us</NavLink>
             <NavLink to={'/faq'} className={({ isActive }) => isActive ? 'pb-4 border-b-2  border-b-buttons' : 'pb-4 hover:border-b-2'}>FAQ</NavLink>
+            <NavLink to={'/about-us'} className={({ isActive }) => isActive ? 'pb-4 border-b-2  border-b-buttons' : 'pb-4 hover:border-b-2'}>About Us</NavLink>
         </section>
 
-        <section className='flex cursor-pointer pb-4 gap-5'>
-            <section className='flex items-center gap-2 bg-inputs w-80 p-2 rounded-xl'>
+        <section className='flex flex-col w-screen items-center align-middle m-auto  cursor-pointer pb-4 gap-5  md:flex-row md:justify-center'>
+            <section className='flex items-center gap-2 bg-inputs w-72 md:w-1/3 p-2 rounded-xl'>
                 <CiSearch/>
-            <input className='bg-inputs outline-none border-none' type="text" name="search" id="search" placeholder="Search an item" />
+            <input className='bg-inputs outline-none border-none w-full' type="text" name="search" id="search" placeholder="Search" />
             </section>
           <section className=' flex list-none gap-3 items-center '>
               <li className='p-2 rounded-full bg-inputs'><HiOutlineMail/></li>
               <li className='p-2 rounded-full bg-inputs'><BiNotification/></li>
               <li className='p-2 rounded-full bg-inputs'><BiWallet/></li>
           </section>
-
-            {
-              (login)
-                ? (
-                <section onClick={handleClip} className='flex gap-2 items-center relative hover:shadow-lg'>
-                <ReactNiceAvatar style={{ width: '2rem', height: '2rem' }} {...myConfig} />
-
-                  <p className='text-sm font-bold'>{email}</p>
-                    {
-                      isHovered && (
-                        <div className='absolute -bottom-10 right-0 shadow-lg rounded-md w-full text-center h-full bg-white'>
-                          <button className='flex items-center justify-between w-full h-full font-bold p-2 shadow-xl border-t' onClick={handleLogout}>
-                          Logout
-                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
-                          </svg>
-
-                            </button>
-                          </div>
-                      )
-                    }
-
-                </section>
-                  )
-                : (
-                <section className='flex gap-2 items-center'>
-
-                  <Link to={'/login'} className='text-sm font-bold'>Login</Link>
-
-                </section>
-                  )
-            }
+            <section className='hidden md:block'>
+            <Profile login={login} isHovered={isHovered} handleClip={handleClip} email={email} handleLogout={handleLogout}/>
+            </section>
           </section>
 
     </header>
---
+++ src/pages/Home/components/Profile.tsx
@@ -0,0 +1,66 @@
+import ReactNiceAvatar, { genConfig, type AvatarFullConfig } from 'react-nice-avatar'
+import { Link } from 'react-router-dom'
+
+interface profileProps {
+  login: boolean
+  isHovered: boolean
+  handleClip: () => void
+  email: string | null
+  handleLogout: () => void
+}
+const config: AvatarFullConfig = {
+  sex: 'woman',
+  faceColor: '#F9C9B6',
+  earSize: 'big',
+  eyeStyle: 'smile',
+  noseStyle: 'round',
+  mouthStyle: 'smile',
+  shirtStyle: 'short',
+  glassesStyle: 'none',
+  hairColor: '#000',
+  hairStyle: 'womanLong',
+  hatStyle: 'none',
+  hatColor: '#fff',
+  eyeBrowStyle: 'upWoman',
+  shirtColor: '#F4D150',
+  bgColor: '#FFEDEF'
+}
+
+export const Profile = ({ login, isHovered, handleClip, email, handleLogout }: profileProps): JSX.Element => {
+  const myConfig = genConfig(config)
+  return (
+    <>
+    {
+              (login)
+                ? (
+                <section onClick={handleClip} className='flex gap-2 items-center relative hover:shadow-lg'>
+                <ReactNiceAvatar style={{ width: '2rem', height: '2rem' }} {...myConfig} />
+
+                  <p className='text-sm font-bold'>{email}</p>
+                    {
+                      isHovered && (
+                        <div className='absolute -bottom-10 right-0 shadow-lg rounded-md w-full text-center h-full bg-white'>
+                          <button className='flex items-center justify-between w-full h-full font-bold p-2 shadow-xl border-t' onClick={handleLogout}>
+                          Logout
+                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
+                          </svg>
+
+                            </button>
+                          </div>
+                      )
+                    }
+
+                </section>
+                  )
+                : (
+                <section className='flex gap-2 items-center'>
+
+                  <Link to={'/login'} className='text-sm font-bold'>Login</Link>
+
+                </section>
+                  )
+            }
+    </>
+  )
+}
