304ca8161cc31d2abc1934d78a8ec3a4d9bf1d42
Hector <torrezhectorb@gmail.com>
2023-07-24 19:34:16 -0600

    custom hook to clase when click outside

---
+++ src/hooks/useClose.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useRef, useState } from 'react'
+
+interface Props {
+  dependencies: React.DependencyList
+}
+
+  type UseClickedOutsideReturn = [
+    boolean,
+    React.MutableRefObject<HTMLDivElement | null>,
+
+  ]
+
+export const useClickedOutside = ({
+  dependencies
+}: Props): UseClickedOutsideReturn => {
+  const componentRef = useRef<HTMLDivElement | null>(null)
+  const [clickedOutside, setClickedOutside] = useState(false)
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent): void => {
+      const clickOutside =
+          ((componentRef.current != null) &&
+            !componentRef.current.contains(event.target as Node)) ||
+          false
+
+      setClickedOutside(clickOutside)
+    }
+    document.addEventListener('click', handleClickOutside)
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside)
+    }
+  }, [...dependencies])
+
+  return [clickedOutside, componentRef]
+}
