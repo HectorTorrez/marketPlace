cb76fd6cd455b09cf4edaaf257c4814d28f0922b
Hector <torrezhectorb@gmail.com>
2023-07-24 19:33:06 -0600

    add new components

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -50,3 +50,19 @@ export const Chevrondown = (): JSX.Element => {
 
   )
 }
+
+export const ErrorMessage = (): JSX.Element => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z" />
+    </svg>
+  )
+}
+
+export const SuccessMessage = (): JSX.Element => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+    </svg>
+  )
+}
