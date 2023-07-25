5f7d1f1d98d5444c82da78539eaf3104d5758797
Hector <torrezhectorb@gmail.com>
2023-07-24 19:33:38 -0600

    feature/ create a component and edit styles

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,11 +1,15 @@
+import { ErrorMessage } from './icons'
+
 interface ErrorProps {
   text: string
 }
 
 export const Error = ({ text }: ErrorProps): JSX.Element => {
   return (
-    <div className="bg-red-100 border border-red-400 text-red-700 px-10 py-10 rounded relative mb-5" role="alert">
-    <strong className="font-bold mr-3">Holy smokes!</strong>
+    <div className="flex flex-col items-center gap-3 bg-red-100 border border-red-400 text-red-700 px-10 py-5 rounded relative md:mb-5" role="alert">
+    <strong className="font-bold mr-3 flex items-center">
+      <ErrorMessage/>
+      Error Message</strong>
       <span className="block sm:inline">{text}</span>
       <span className="absolute top-0 bottom-0 right-0 px-4 py-3"></span>
   </div>
---
+++ src/components/Success.tsx
@@ -0,0 +1,16 @@
+import { SuccessMessage } from './icons'
+
+interface SuccessProps {
+  text: string
+}
+
+export const Success = ({ text }: SuccessProps): JSX.Element => {
+  return (
+    <div className="flex flex-col absolute items-center gap-3 bg-green-100 border border-green-400 text-green-700 px-10  rounded top-10 md:mb-5" role="alert">
+    <strong className="font-bold  flex items-center">
+      <SuccessMessage/>
+      {text}</strong>
+      <span className="absolute top-0 bottom-0 right-0 px-4 py-3"></span>
+  </div>
+  )
+}
