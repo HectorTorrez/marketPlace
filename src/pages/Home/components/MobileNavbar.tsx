1050263f5dbd5bcd800dc2fd1c1f4502bbc4bac6
Hector <torrezhectorb@gmail.com>
2023-08-08 20:06:01 -0600

    style

--- src/pages/Home/components/MobileNavbar.tsx
+++ src/pages/Home/components/MobileNavbar.tsx
@@ -1,7 +1,7 @@
 
 export const MobileNavbar = ({ children }: { children: React.ReactNode }): JSX.Element => {
   return (
-    <section className='absolute w-72 right-0 bg-white  flex flex-col items-center align-middle gap-10 px-4 py-4 min-w-60 md:hidden'>
+    <section className='z-50 absolute w-72 right-0 bg-white  flex flex-col items-center align-middle gap-10 px-4 py-4 min-w-60 md:hidden'>
     { children }
     </section>
   )
