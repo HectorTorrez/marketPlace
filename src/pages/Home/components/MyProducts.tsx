c2cc2d2ba99a251887f0e53fa418234430f6eba1
Hector <torrezhectorb@gmail.com>
2023-08-14 19:11:28 -0600

    new conditional

--- src/pages/Home/components/MyProducts.tsx
+++ src/pages/Home/components/MyProducts.tsx
@@ -2,24 +2,35 @@ import { useSelector } from 'react-redux'
 import { DesktopNavbar } from '.'
 import { Product } from './Product'
 import { type RootState } from '../../../store'
+import { Info } from '../../../components/Info'
 export const MyProducts = (): JSX.Element => {
   const product = useSelector((state: RootState) => state.product.products)
   const id = useSelector((state: RootState) => state.auth.userId)
+  const isSignedIn = useSelector((state: RootState) => state.auth.login)
 
   return (
     <>
       <DesktopNavbar/>
-      <section className='grid  md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 max-w-4xl m-auto mb-10'>
       {
-        product.map(item => {
-          if (item.user_id === id) {
-            return <Product key={item.id} {...item}/>
+        isSignedIn
+          ? (
+          <section className='grid  md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 max-w-4xl m-auto mb-10'>
+          {
+            product.map(item => {
+              if (item.user_id === id) {
+                return <Product key={item.id} {...item}/>
+              }
+              return null
+            }
+            )
           }
-          return null
-        }
-        )
+          </section>
+
+            )
+          : (
+          <Info text='Login to watch your products'/>
+            )
       }
-      </section>
     </>
   )
 }
